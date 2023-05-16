216696aaed9724cccf6e2431d1dbf2663b4d7c06
jgsamartin <jgsamartin@gmail.com>
2023-05-16 19:48:23 +0200

    Mejoras clase. readCameraArduino adaptado a clases

--- Code_matlab/readCameraArduino.m
+++ Code_matlab/readCameraArduino.m
@@ -5,24 +5,22 @@
 % 2023-05-16
 
 %% Configuración
-obj = serialport('COM3', 9600);
-millisdesinflar = "w,1,-5000,-5000,-5000,-5000,-5000,-5000,-5000,-5000,-5000";
-writeline(obj, millisdesinflar);
-k = 1.5;        % Factor de deshinchado
-pause(5);
+r = Robot();
+r.Connect();
+r.Deflate();
 
 %% Calibración
-writeOneValveMillis(obj, 0, 400);
+r.writeOneValveMillis(0, 400);
 pause(2);
-writeOneValveMillis(obj, 0, -k*400);
+r.writeOneValveMillis(0, -400);
 pause(1);
-writeOneValveMillis(obj, 1, 400);
+r.writeOneValveMillis(1, 400);
 pause(2);
-writeOneValveMillis(obj, 1, -k*400);
+r.writeOneValveMillis(1, -400);
 pause(1);
-writeOneValveMillis(obj, 2, 400);
+r.writeOneValveMillis(2, 400);
 pause(2);
-writeOneValveMillis(obj, 2, -k*400);
+r.writeOneValveMillis(2, -400);
 pause(1);
 
 %% Captura de datos
