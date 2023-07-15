22203888d3e104103f453408c90bf48b00d5d578
rulox27 <raulmg2731@gmail.com>
2023-07-15 19:11:01 +0200

    Modificaciones NN

--- Code_matlab/Neural_Network_Calibration.m
+++ Code_matlab/Neural_Network_Calibration.m
@@ -1,4 +1,4 @@
-MAX_MILLIS = 1200;
+MAX_MILLIS = 1000;
 N_ITER_MAX = 1000;
 SAVE = 100;
 
@@ -11,52 +11,65 @@ millis_ant = [0 0 0];
 
 prueba = zeros(N_ITER_MAX,3);
 
+R.Calibrate();
 
 for i = 1:N_ITER_MAX
    
-    t(i,:) = -MAX_MILLIS -10 + 10*randi(fix((2*MAX_MILLIS/10 + 1)), [1 3]);
+    while 1
 
-    if i == 1
-        suma = t(i,:);
-    else
-        suma = suma + t(i,:);
-    end
+        t(i,:) = -MAX_MILLIS -20 + 20*randi(fix((2*MAX_MILLIS/20 + 1)), [1 3]);
+    
+        if i == 1
+            suma = t(i,:);
+        else
+            suma = suma + t(i,:);
+        end
+    
+        for j = 1:3
+            if suma(j) < 0
+                suma(j) = 0;
+            end 
+        end
+    
+        index = find(suma > MAX_MILLIS);
+    
+        if ~isempty(index)
+            suma(1,index) = suma(1,index) - t(i,index);
+            t(i,index) = 0;
+        end
+    
+        indice = find(t(i,:) == min(t(i,:)));
+    
+        if (suma(indice(1)) ~= 0) && (isempty(find(suma == 0, 1)))
+            t(i,indice) = -suma(indice);
+            suma(indice) = 0;
+        end
 
-    for j = 1:3
-        if suma(j) < 0
-            suma(j) = 0;
-        end 
-    end
-
-    index = find(suma > MAX_MILLIS);
-
-    if ~isempty(index)
-        suma(1,index) = suma(1,index) - t(i,index);
-        t(i,index) = 0;
-    end
-
-    indice = find(t(i,:) == min(t(i,:)));
-
-    if (suma(indice(1)) ~= 0) && (isempty(find(suma == 0, 1)))
-        t(i,indice) = -suma(indice);
-        suma(indice) = 0;
+        if isempty(find(ismember(prueba(1:i-1,:),suma,'rows') == 1,1))
+            break;
+        end
     end
 
     prueba(i,:) = suma;
     
 
     R.WriteSegmentMillis(t(i,:));
-    pause(2 * MAX_MILLIS / 1000)
+    pause(2.5 * MAX_MILLIS / 1000)
     R.Measure();
     pause(0.05)
 
     vol_raw = R.getVoltages();
+    
+    for n = 1:3
+        vol_raw(n) = 100.0000 - (vol_raw(n) - R.max_min(n,1)) * 100.0000 / (R.max_min(n,2) - R.max_min(n,1));
+    end
+
     pos_raw = R.CapturePosition();
     pos(i,:) = pos_raw(2,:);
     vol(i,:) = vol_raw';
 
     if ~mod(i,SAVE)
-        save(strcat('DatasetNN/prueba_',num2str(i/SAVE)),'pos','vol','t');
+        save(strcat('DatasetNN/prueba2_',num2str(i/SAVE)),'pos','vol','t','prueba');
     end
 
 end
