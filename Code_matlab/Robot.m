ededa7e235a572953a90a73a4a8d3d533f2557fe
rulox27 <raulmg2731@gmail.com>
2023-07-20 18:24:39 +0200

    Mas datos

--- Code_matlab/Robot.m
+++ Code_matlab/Robot.m
@@ -10,7 +10,7 @@ classdef Robot < handle
         serialDevice;                       % Serial port to which Arduino is connected
         realMode = 1;                       % 1 if working with the real robot
         deflatingTime = 1500;               % Default deflating time
-        deflatingRatio = 2.5;               % Relation between deflation and inflation time
+        deflatingRatio = 2.6;               % Relation between deflation and inflation time
         maxAction = 500;
         max_millis = 1000;
         nValves = 9;                        % Number of valves
